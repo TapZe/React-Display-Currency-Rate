53634f7122543908bd900d3e130eae270a0b0420
TapZe <nabilmuyassar@Gmail.com>
2024-09-24 17:21:01 +0700

    style: Edited styling for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ const Footer = () => {
     <footer className="bg-gray-800 text-gray-200 py-3 text-center">
       <p>&copy; 2024 Currency Tracker. All rights reserved.</p>
       <p>
-        Developed by <a href="https://linkedin.com/in/muhammad-nabil-muyassar-rahman" className="text-yellow-400 hover:underline">Muhammad Nabil</a>
+        Developed by <a href="https://linkedin.com/in/muhammad-nabil-muyassar-rahman" className="text-yellow-400 hover:underline">Muhammad Nabil Muyassar Rahman</a>
+        {" "}[1959095840-51].
       </p>
     </footer>
   );
